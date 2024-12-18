ef214e09009db20e138fde7fe69624dc89b4fbf8
nmnnrb <151563433+nmnnrb@users.noreply.github.com>
2024-12-18 14:02:36 +0530

    final-push

--- Backend/server.js
+++ Backend/server.js
@@ -28,9 +28,23 @@ app.use('/api/user' , userRouter)
 app.use('/api/product' , productRouter)
 app.use('/api/cart' , cartRouter);
 app.use('/api/order' , orderRouter)
-app.get('/' ,(req, res) =>{
-    res.send('API working , For the E-Commerce Project - by NamanSharma')
-})
+app.get('/', (req, res) => {
+    res.send(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>E-Commerce Project</title>
+           <script src="https://cdn.tailwindcss.com"></script>
+        </head>
+        <body class="bg-gray-100 flex flex-col justify-center items-center item-center text-center  container h-screen p-8">
+          <h1 class="text-5xl text-indigo-600  font-bold">API Working</h1>
+          <p class="text-xl text-black  mt-2 font-bold" >For the E-Commerce Project - by Naman Sharma</p>
+          <p class="text-3xl text-green-500  mt-6 font-bold" >Now you checkout the client <a class="bg-blue-500 hover:bg-blue-600 text-white px-3 py-3 rounded-xl"  href="https://e-commerce-4zy9.onrender.com/"> click here </a> </p>
+        </body>
+      </html>
+    `);
+  });
+  
 
 
 app.listen(port, () => {
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
     useEffect(() => {
       const timer = setTimeout(() => {
         setShowPopup(false);
-      }, 15000);
+      }, 30000);
       return () => clearTimeout(timer);
     }, []);
   
@@ -33,7 +33,7 @@ const Home = () => {
           <div className="bg-white p-6 rounded-lg shadow-lg text-center max-w-md">
             <h2 className="text-2xl font-bold text-red-600 mb-4">**ATTENTION!!</h2>
             <p className="text-gray-600 mb-6">
-              For testing purposes, make sure the backend is up and running.<a className='px-2 py-1 text-white bg-blue-600 hover:bg-blue-400 rounded-md' href='https://e-commerce-backend-8x0r.onrender.com'> Check Backend</a>  As the project is hosted on the free server, it will take some time to fully work in production.
+              For testing purposes, make sure the backend is up and running.<a className='px-2 py-1 text-white bg-blue-600 hover:bg-blue-400 rounded-md' href='https://e-commerce-backend-8x0r.onrender.com'> Check here to check backend   </a>  As the project is hosted on the free server, it will take few second to minutes <span className='text-white text-sm rounded-md font-light bg-black px-2 py-1 ' >usually it make take 40-60 second to run backend</span> time to fully work in production.
             </p>
             <button
               onClick={handleClosePopup}
